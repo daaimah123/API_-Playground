62006ed62f9587123ea516a8e40f5376104ec975
Daaimah Tibrey <dtibrey@surveymonkey.com>
2019-09-01 18:30:48 -0700

    movie search express app

---
+++ movie_search_app/app.js
@@ -0,0 +1,16 @@
+//general search
+'http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb'
+//movie id
+'http://www.omdbapi.com/?i=tt3896198&apikey=thewdb'
+
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.get()
+
+//start server with node app.js
+app.listen(process.env.PORT, () => {
+    console.log("<<<<<<<<===Server running on port 3000===>>>>>>>");
+   });
+   
