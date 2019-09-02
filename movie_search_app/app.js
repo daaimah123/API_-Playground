3b884e446de20c104e136dbdde7646097272c33b
Daaimah Tibrey <dtibrey@surveymonkey.com>
2019-09-01 18:52:52 -0700

    add results route get single CA movie result

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -3,14 +3,22 @@
 //movie id
 'http://www.omdbapi.com/?i=tt3896198&apikey=thewdb'
 
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var request = require('request');
 
-app.get()
+app.get('/results', (req, res) => {
+    request('http://www.omdbapi.com/?s=california&apikey=thewdb', (error, response, body) => {
+        if (!error && response.statusCode === 200){
+            var data = JSON.parse(body);
+            res.send(data['Search'][0])
+        }
+    });
+});
 
 //start server with node app.js
 app.listen(process.env.PORT, () => {
-    console.log("<<<<<<<<===Server running on port 3000===>>>>>>>");
+    console.log(`<<<<<<<<===Server running on port ${process.env.PORT}===>>>>>>>`);
    });
    
